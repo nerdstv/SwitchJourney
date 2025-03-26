5b288a7a9e727673f290bc34c4cad36d0b6881a7
Satvik <satvikshrivastava12gmail.com>
2025-03-27 00:54:13 +0530

    Day3.1

--- DSA/Codes/Arrays.cpp
+++ DSA/Codes/Arrays.cpp
@@ -147,7 +147,79 @@ Note: In the second question, there can be multiple majority elements so we have
 To check the frequency of the candidate element, we have to traverse the array again.
 
 */
+class Solution {
+    public:
+        int majorityElement(vector<int>& nums) {
+            int n = nums.size();
+            int ans = nums[0];
+            int count = 1;
+    
+            for(int i = 1; i < n; i++){
+    
+                if(count == 0){
+                    count = 1;
+                    ans = nums[i];
+                }
+                else if(nums[i] == ans) count++;
+                else {
+                    //ans = nums[i];
+                    count --; 
+                }
+            }
+    
+            return ans;
+            
+        }
+    };
 
+    
+class Solution {
+    public:
+        vector<int> majorityElementII(vector<int>& nums) {
+            int n = nums.size();
+            
+            int maj1    = NULL;
+            int count1 = 0;
+    
+            int maj2    = NULL;
+            int count2 = 0;
+            int freq = floor(n/3);
+            
+            for(int i = 0; i<n; i++) {
+                if(nums[i] == maj1)
+                    count1++;
+                else if(nums[i] == maj2)
+                    count2++;
+                else if(count1 == 0) {
+                    maj1 = nums[i];
+                    count1 = 1;
+                } else if(count2 == 0) {
+                    maj2 = nums[i];
+                    count2 = 1;
+                } else {
+                    count1--;
+                    count2--;
+                }
+            }
+            
+            vector<int> result; //because atmost two elements can occur more than ⌊ n/3 ⌋ in an array of length n
+            count1 = 0;
+            count2 = 0;
+            for(int num:nums) {
+                if(num == maj1)
+                    count1++;
+                else if(num == maj2)
+                    count2++;
+            }
+            if(count1 > floor(n/3))
+                result.push_back(maj1);
+            if(count2 > floor(n/3))
+                result.push_back(maj2);
+            return result;
+    
+    
+        }
+    };
 
 
 /*
